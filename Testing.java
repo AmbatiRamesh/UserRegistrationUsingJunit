7ff699347926534a83f98a3b90d005522353f03c
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-03-04 23:02:05 +0530

    Added: message after every test case using Junit

--- Testing.java
+++ Testing.java
@@ -1,21 +1,70 @@
 package com.junit;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class Testing {
-    @BeforeEach
-    void Message(){
+    @BeforeAll
+    static void beforeAllTestMsg() {
         System.out.println("-----Welcome to User Registration Problem-----");
     }
     @AfterAll
-    public static void afterMessage(){
-        System.out.println("Passed");
+    static void afterAllTestPassedMsg(){
+        System.out.println("All Test Case Passed");
+    }
+    @AfterEach
+    public void afterEachTestPassMsg() {
+        System.out.println("Happy Test Case");
+    }
+    @BeforeEach
+    public void beforeEachTestPassMsg(){
+        System.out.println("Sad Test Case");
+    }
+    @Test
+    public void givenFirstNameShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        assertEquals(true, userRegistration.checkFirstName("Ramesh"));
+    }
+    @Test
+    public void givenLastNameShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        assertEquals(true, userRegistration.checkLastName("Ambati"));
+    }
+    @Test
+    public void givenEmailIdShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        assertEquals(true, userRegistration.checkEmailId("rameshambati.ab@gmail.com"));
+    }
+    @Test
+    public void givenPhoneNumberShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        assertEquals(true, userRegistration.checkPhoneNumber("91 9640827483"));
+    }
+    @Test
+    public void givenPasswordRule1ShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        assertEquals(true, userRegistration.password("viratkohli"));
+    }
+    @Test
+    public void givenPasswordRule2ShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        assertEquals(true, userRegistration.password2("msdhoni"));
     }
     @Test
     public void givenPasswordRule3ShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
+        assertEquals(true, userRegistration.password3("Rohitsharma45"));
+    }
+    @Test
+    public void givenPasswordRule4ShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        assertEquals(true, userRegistration.password4("Rohitsharma45@"));
+    }
+    @Test
+    public void givenEmailSeperateShouldReturnTrue() {
+       UserRegistration userRegistration = new UserRegistration();
         assertEquals(true, userRegistration.checkEmailSeperate("abc@yahoo.com"));
     }
 }
--- UserRegistration.java
+++ UserRegistration.java
@@ -2,10 +2,41 @@ package com.junit;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+    public static final String VALIDATE_NAME="^[A-z]+[a-z]{2,}$";
+    public boolean checkFirstName(String firstName) {
+        return (Pattern.matches(VALIDATE_NAME, firstName));
+    }
+    public static final String VALIDATE_NAME1="^[A-z]+[a-z]{2,}$";
+    public boolean checkLastName(String lastName) {
+        return (Pattern.matches(VALIDATE_NAME1, lastName));
+    }
+    public static final String  EMAIL_ID="^[A-Za-z]{3,}[.]?[A-Za-z]{2,}[@]{1}[a-z]{2,}[.]{1}[a-z]{2,}$";
+    public boolean checkEmailId(String emailId) {
+        return (Pattern.matches(EMAIL_ID, emailId));
+    }
+    public static final String PHONE_NUMBER="^[0-9]+\s[0-9]{10}$";
+    public Boolean checkPhoneNumber(String phoneNumber) {
+        return (Pattern.matches(PHONE_NUMBER,phoneNumber));
+    }
+    public static final String PASSWORD_RULE1="^[a-z]{8,}$";
+    public Boolean password(String password) {
+        return (Pattern.matches(PASSWORD_RULE1,password));
+    }
+    public static final String PASSWORD_RULE2="^[A-z]+[a-z]{2,}$";
+    public Boolean password2(String password2) {
+        return (Pattern.matches(PASSWORD_RULE2,password2));
+    }
+    public static final String PASSWORD_RULE3="^[A-Z]+[a-z0-9]{7,}$";
+    public Boolean password3(String password3) {
+        return (Pattern.matches(PASSWORD_RULE3,password3));
+    }
+    public static final String PASSWORD_RULE4="^[A-Z]+[a-z0-9]{7,}[!@#$%&*]+$";
+    public Boolean password4(String password4) {
+        return (Pattern.matches(PASSWORD_RULE4,password4));
+    }
     public static final String  SEPERATE_EMAIL_ID="^[A-Za-z]{3,}[.+-]?[A-Za-z0-9]*[@]{1}[a-z0-9]{1,}[.]{1}[a-z]{2,}[.]?[a-z]*$";
-
     public Boolean checkEmailSeperate(String checkEmailSeperate) {
         return (Pattern.matches(SEPERATE_EMAIL_ID,checkEmailSeperate));
         }
-    }
+}
 
