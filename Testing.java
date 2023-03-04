04c8266d6ce65610f7d16575bb2e32258e288353
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-03-04 14:28:56 +0530

    Added: Validate first name using Junit

---
+++ Testing.java
@@ -0,0 +1,22 @@
+package com.junit;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Testing {
+    @BeforeEach
+    void Message(){
+        System.out.println("-----Welcome to User Registration Problem-----");
+    }
+    @AfterAll
+    public static void afterMessage(){
+        System.out.println("Passed");
+    }
+    @Test
+    public void givenFirstNameShouldReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        assertEquals(true, userRegistration.checkFirstName("Ramesh"));
+    }
+}
+
---
+++ UserRegistration.java
@@ -0,0 +1,10 @@
+package com.junit;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+        public static final String VALIDATE_NAME="^[A-z]+[a-z]{2,}$";
+        public boolean checkFirstName(String firstName) {
+            return (Pattern.matches(VALIDATE_NAME, firstName));
+        }
+    }
+
