6cfde6fc6450b59fbe3d695359d559b2151c1323
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-03-05 23:19:14 +0530

    Added: Refactor the Code to throw custom exceptions in case of Invalid User Details

--- Testing.java
+++ Testing.java
@@ -1,33 +1,70 @@
 package com.junit;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
-import java.util.Arrays;
-import java.util.Collection;
-
-@RunWith(Parameterized.class)
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Testing {
-    private final String email;
-    private final Boolean expectedResult;
-    public UserRegistration emailValidation;
-    public Testing(String email, Boolean expectedResult) {
-        this.email = email;
-        this.expectedResult = expectedResult;
+    @BeforeAll
+    static void beforeAllTestMsg() {
+        System.out.println("-----Welcome to User Registration Problem-----");
     }
-    @Before
-    public void initilize() {
-        emailValidation = new UserRegistration();
+    @AfterAll
+    static void afterAllTestPassedMsg(){
+        System.out.println("All Test Case Passed");
     }
-    @Parameterized.Parameters
-    public static Collection input() {
-        return Arrays.asList(new Object[][]{{"abc@yahoo.com", true}, {"abc-100@yahoo.com", true}, {"abc.100@yahoo.com", true}, {"abc111@abc.com", true}, {"abc-100@abc.net", true}, {"abc.100@abc.com.au", true}, {"abc@1.com", true}, {"abc@gmail.com.com", true}, {"abc+100@gmail.com", true}});
+    @AfterEach
+    public void afterEachTestPassMsg() {
+        System.out.println("Happy Test Case");
+    }
+    @BeforeEach
+    public void beforeEachTestPassMsg(){
+        System.out.println("Sad Test Case");
     }
     @Test
-    public void testEmailValidation() {
-        System.out.println(email + " is valid");
-        Assert.assertEquals(expectedResult, emailValidation.checkEmail(email));
+    public void givenFirstNameShouldReturnTrue() throws UserRegistration.UserValidationException {
+        UserRegistration userRegistration = new UserRegistration();
+        assertEquals(true, userRegistration.checkFirstName("Ramesh"));
+    }
+    @Test
+    public void givenLastNameShouldReturnTrue() throws UserRegistration.UserValidationException {
+        UserRegistration userRegistration = new UserRegistration();
+        assertEquals(true, userRegistration.checkLastName("Ambati"));
+    }
+    @Test
+    public void givenEmailIdShouldReturnTrue() throws UserRegistration.UserValidationException {
+        UserRegistration userRegistration = new UserRegistration();
+        assertEquals(true, userRegistration.checkEmailId("rameshambati.ab@gmail.com"));
+    }
+    @Test
+    public void givenPhoneNumberShouldReturnTrue() throws UserRegistration.UserValidationException {
+        UserRegistration userRegistration = new UserRegistration();
+        assertEquals(true, userRegistration.phoneNumber("91 9640827483"));
+    }
+    @Test
+    public void givenPasswordRule1ShouldReturnTrue() throws UserRegistration.UserValidationException {
+        UserRegistration userRegistration = new UserRegistration();
+        assertEquals(true, userRegistration.password("viratkohli"));
+    }
+    @Test
+    public void givenPasswordRule2ShouldReturnTrue() throws UserRegistration.UserValidationException {
+        UserRegistration userRegistration = new UserRegistration();
+        assertEquals(true, userRegistration.password2("Msdhonii"));
+    }
+    @Test
+    public void givenPasswordRule3ShouldReturnTrue() throws UserRegistration.UserValidationException {
+        UserRegistration userRegistration = new UserRegistration();
+        assertEquals(true, userRegistration.password3("Rohitsharma45"));
+    }
+    @Test
+    public void givenPasswordRule4ShouldReturnTrue() throws UserRegistration.UserValidationException {
+        UserRegistration userRegistration = new UserRegistration();
+        assertEquals(true, userRegistration.password4("Rohitsharma45@"));
+    }
+    @Test
+    public void givenEmailSeperateShouldReturnTrue() throws UserRegistration.UserValidationException {
+        UserRegistration userRegistration = new UserRegistration();
+        assertEquals(true, userRegistration.checkEmailSeperate("abc@yahoo.com"));
     }
 }
--- UserRegistration.java
+++ UserRegistration.java
@@ -1,10 +1,79 @@
 package com.junit;
 import java.util.regex.Pattern;
-
-public class UserRegistration {
-        public static final String SEPERATE_EMAIL_ID="^[A-Za-z]{3,}[.+-]?[A-Za-z0-9]*[@]{1}[a-z0-9]{1,}[.]{1}[a-z]{2,}[.]?[a-z]*$";
-        public Boolean checkEmail(String email){
-            Boolean expectedResult=(Pattern.matches(SEPERATE_EMAIL_ID,email));
-            return expectedResult;
+public class UserRegistration{
+public static final String VALIDATE_NAME="^[A-Z]+[a-z]{2,}$";
+public static final String EMAIL_ID="^[A-Za-z]{3,}[.-]?[A-Za-z]{2,}[@]{1}[a-z]{2,}[.]{1}[a-z]{2,}$";
+public static final String PHONE_NUMBER="^[0-9]+\s[0-9]{10}$";
+public static final String PASSWORD_RULE1="^[a-z]{8,}$";
+public static final String PASSWORD_RULE2="^[A-Z]+[a-z]{7,}$";
+public static final String PASSWORD_RULE3="^[A-Z]+[a-z0-9]{7,}$";
+public static final String PASSWORD_RULE4="^[A-Z]+[a-z0-9]{7,}[!@#$%&*]+$";
+public static final String SEPERATE_EMAIL_ID="^[A-Za-z]{3,}[.+-]?[A-Za-z0-9]*[@]{1}[a-z0-9]{1,}[.]{1}[a-z]{2,}[.]?[a-z]*$";
+    public class UserValidationException extends Exception {
+        public UserValidationException(String invalid_firstName) {
+            super(invalid_firstName);
         }
     }
+public Boolean checkFirstName(String firstName) throws UserValidationException {
+        if (Pattern.matches(VALIDATE_NAME, firstName))
+        return true;
+        else
+        throw new UserValidationException("Invalid FirstName , Please Enter Valid FirstName");
+        }
+
+public Boolean checkLastName(String lastName) throws UserValidationException {
+        if (Pattern.matches(VALIDATE_NAME,lastName))
+        return true;
+        else
+        throw new UserValidationException("Invalid Last Name , Please Enter Valid LastName");
+        }
+
+public Boolean checkEmailId(String emailId) throws UserValidationException {
+        if (Pattern.matches(EMAIL_ID,emailId))
+        return true;
+        else
+        throw new UserValidationException("Invalid Email ID, Please Enter Valid Email ID");
+        }
+
+public Boolean phoneNumber(String phoneNumber) throws UserValidationException {
+        if (Pattern.matches(PHONE_NUMBER,phoneNumber))
+        return true;
+        else
+        throw new UserValidationException("Invalid PhoneNumber, Please Enter Valid PhoneNumber");
+        }
+
+public Boolean password(String password) throws UserValidationException {
+        if (Pattern.matches(PASSWORD_RULE1,password))
+        return true;
+        else
+        throw new UserValidationException("Invalid PasswordRule1, Please Enter Valid Password");
+        }
+
+public Boolean password2(String password1) throws UserValidationException {
+        if(Pattern.matches(PASSWORD_RULE2,password1))
+        return true;
+        else
+        throw new UserValidationException("Invalid PasswordRule2, Please Enter Valid Password");
+        }
+
+public Boolean password3(String password2) throws UserValidationException {
+        if (Pattern.matches(PASSWORD_RULE3,password2))
+        return true;
+        else
+        throw new UserValidationException("Invalid PasswordRule3, Please Enter Valid Password");
+        }
+
+public Boolean password4(String password3) throws UserValidationException {
+        if (Pattern.matches(PASSWORD_RULE4,password3))
+        return true;
+        else
+        throw new UserValidationException("Invalid PasswordRule4, Please Enter Valid Password");
+        }
+
+public Boolean checkEmailSeperate(String checkEmailSeperate) throws UserValidationException {
+        if (Pattern.matches(SEPERATE_EMAIL_ID,checkEmailSeperate))
+        return true;
+        else
+        throw new UserValidationException("Invalid Email, Please Enter Valid Email");
+        }
+}
